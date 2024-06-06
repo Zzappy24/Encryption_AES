5c512854f147e17d9cff52867f38912c7ab3df68
ysapienza <ysapienza@VDIDEVDATA007>
2024-06-06 12:08:31 +0200

    rectification chemin fichier

--- decrypt_main.py
+++ decrypt_main.py
@@ -4,7 +4,7 @@ import sys
 from function import decrypt_AES_GCM, read_file
 
 if __name__ == "__main__":
-    encryptMsg = read_file("./encryptMessage/message.txt")
+    encryptMsg = read_file("./encryptMessage/message.json")
     decryptMsg = decrypt_AES_GCM(encryptMsg, sys.argv[1])
     print(decryptMsg)
 
--- encrypt_main.py
+++ encrypt_main.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     encryptMsg = encrypt_AES_GCM(msg=b'test cryptage AES',
                                  secretKey=sys.argv[1])
     print(encryptMsg)
-    save_encrypt_message(encryptMsg, "encryptMessage/message.txt")
+    save_encrypt_message(encryptMsg, "encryptMessage/message.json")
